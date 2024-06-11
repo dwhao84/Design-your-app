463aed8c8af24e9fbbcc070fd45420a07572fcb7
Dawei Hao <dwsamurai84@gmail.com>
2024-06-12 06:22:12 +0800

    Initial Commit

---
+++ Design your app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Design your app
+//
+//  Created by Dawei Hao on 2024/6/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Design your app/Design_your_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Design_your_appApp.swift
+//  Design your app
+//
+//  Created by Dawei Hao on 2024/6/12.
+//
+
+import SwiftUI
+
+@main
+struct Design_your_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
