13c1f07696d4188a14fee58a63fde1d40c853cb3
Dawei Hao <dwsamurai84@gmail.com>
2024-06-12 10:44:45 +0800

    Add information data.

---
+++ Design your app/Data.swift
@@ -0,0 +1,21 @@
+//
+//  Data.swift
+//  Design your app
+//
+//  Created by Dawei Hao on 2024/6/12.
+//
+
+import Foundation
+import SwiftUI
+
+struct Info {
+    let content: String
+}
+
+let infomation: [Info] = [
+    Info(content: "I am thankful for the support of my family and friends."),
+    Info(content: "Feeling excited about the new opportunities ahead."),
+    Info(content: "Joyful moments are often found in the simplest things."),
+    Info(content: "Grateful for the kindness of strangers."),
+    Info(content: "Excited to start a new chapter in my life.")
+]
